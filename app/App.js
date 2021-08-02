d3fd17f9996db99c1eeeda83938f232d3c93b73a
David <drios@denwaip.com>
2021-08-02 04:11:43 -0300

    todo app initial commit

---
+++ app/App.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.appBackground}>
+      <ScrollView style={styles.appBackground}>
+        <View style={styles.appBackground} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  appBackground: {
+    backgroundColor: 'white',
+  },
+});
+
+export default App;
